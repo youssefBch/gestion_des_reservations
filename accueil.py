8567d91f8472d8a524db18711c9c35a522237c6b
Youssef bouchti <bouchti.youssef4@gmail.com>
2025-12-12 23:38:46 +0100

    add pages

--- accueil.py
+++ accueil.py
@@ -2,4 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sqlalchemy import text
-conn = st.connection("mydatabase", type="sql")
+from connectionDB import *
+st.set_page_config(page_title = "Welcome To our reservation web site")
+st.title("This is the Home Page.")
+st.sidebar.success("Select Any Page from here")
--- connectionDB.py
+++ connectionDB.py
@@ -2,4 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sqlalchemy import text
-from connectionDB import *
+
+conn = st.connection("mydatabase", type="sql")
