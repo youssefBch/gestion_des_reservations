8d26ca26ffd5d55563c6ab9148f57bed96034ac5
Youssef bouchti <bouchti.youssef4@gmail.com>
2025-12-13 13:50:12 +0100

    add db tables and add map in streamlit library (folium)

--- accueil.py
+++ accueil.py
@@ -6,3 +6,6 @@ from connectionDB import *
 st.set_page_config(page_title = "Welcome To our reservation web site")
 st.title("This is the Home Page.")
 st.sidebar.success("Select Any Page from here")
+
+
+col1,col2,col3 = st.columns(3)
--- pages/1_Agences.py
+++ pages/1_Agences.py
@@ -3,3 +3,6 @@ import pandas as pd
 import numpy as np
 from sqlalchemy import text
 from connectionDB import *
+st.set_page_config(page_title = "Welcome To our agence web site")
+st.title("This is the Home Page.")
+st.sidebar.success("Select Any Page from here")
