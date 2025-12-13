34b7b438226e4f744cdf125d8eefc0be693148e0
Amine El Asri <amine.elasri002@gmail.com>
2025-12-13 22:18:44 +0100

    Add visual separator between agency cards

--- pages/1_Agences.py
+++ pages/1_Agences.py
@@ -5,24 +5,37 @@ from sqlalchemy import text
 import folium
 from streamlit_folium import st_folium
 from connectionDB import *
-st.set_page_config(page_title = "Welcome To our reservation web site")
-st.title("This is the Home Page.")
+st.set_page_config(page_title = "Welcome To agencies page")
+
 st.sidebar.success("Select Any Page from here")
 
 df_agences = conn.query("SELECT * FROM AGENCE_DE_VOYAGE")
 st.subheader("Agences de Voyage Disponibles")
 for index, row in df_agences.iterrows():
-    col1, col2, col3 = st.columns([1, 2, 1])  # ratio des colonnes
+    # Séparateur vide entre les agences
+    st.markdown(f"""
+    <div style="
+        height:10px;                 /* hauteur du séparateur */
+        background-color:#1b3a2d;    /* couleur sombre */
+        margin-bottom:10px;          /* espace avant le contenu */
+        border-radius:5px;           /* coins arrondis */
+    ">
+    </div>
+    """, unsafe_allow_html=True)
+
+    col1, col2, col3 = st.columns([1, 2, 1])
+
     with col1:
         st.markdown(f"**Code Agence :** {row['code_a']}")
+
     with col2:
         st.markdown(f"**Téléphone :** {row['telephone']}")
-        st.markdown(f"**Site Web :** {row['site_web']}")
+        st.markdown(f"**Site Web :** <a href='{row['site_web']}' target='_blank'>{row['site_web']}</a>", unsafe_allow_html=True)
         st.markdown(f"**Adresse :** {row['Adresse_rue_a']}, {row['Adresse_code_postal']}, {row['Adresse_pays_a']}")
         st.markdown(f"**Ville :** {row['VILLE_nom_ville']}")
+
     with col3:
         st.button("Réserver", key=f"btn_{index}")
-    st.markdown("---")  # séparateur entre agences
 
 
 def map():
